e59bef01a1821290fffe430d0fc0cab017dabc8b
MattMcLennan <mclennan6891@gmail.com>
2017-11-18 15:18:35 -0500

    Folder restructure completed

---
+++ InversionIntCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InversionIntCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
