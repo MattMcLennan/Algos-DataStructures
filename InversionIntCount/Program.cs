7e03e134848f847e3faf255457d2812e1dbaca7a
MattMcLennan <mclennan6891@gmail.com>
2017-11-19 19:21:25 -0500

    Bruce Force Method for algo completed in big O of n-squared

--- InversionIntCount/Program.cs
+++ InversionIntCount/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace InversionIntCount
 {
+    internal enum ExitCode : int
+    {
+        Success = 0,
+        InvalidFilename = 1,
+        UnknownError = 10
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static int GetInversions(List<int> inputList)
         {
-            Console.WriteLine("Hello World!");
+            var inversions = 0;
+            var len = inputList.Count - 1;
+
+            for (var i = 0; i <= len; i++)
+            {
+                for (var j = i; j <= len; j++)
+                {
+                    if (inputList[i] > inputList[j])
+                    {
+                        inversions++;
+                    }
+                }
+            }
+
+            return inversions;
+        }
+
+        static int Main(string[] args)
+        {
+            var fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                return (int)ExitCode.InvalidFilename;
+            }
+
+            var totalInversions = 0;
+            foreach(var line in File.ReadLines(@fileName))
+            {
+                totalInversions += GetInversions(ConvertLine(line));
+            }
+
+            Console.WriteLine($"Total Inversions = {totalInversions}");
+            return (int)ExitCode.Success;
+        }
+
+        private static List<int> ConvertLine(string line)
+        {
+            var result = new List<int>();
+            foreach (char i in line)
+            {
+                result.Add(i);
+            }
+
+            return result;
         }
     }
 }
