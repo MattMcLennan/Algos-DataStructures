44bbb809d66dd63794390d820f3a88d6607307bc
MattMcLennan <mclennan6891@gmail.com>
2017-11-20 23:58:56 -0500

    RecursionFunction First Run Done

--- InversionIntCount/Program.cs
+++ InversionIntCount/Program.cs
@@ -32,6 +32,41 @@ namespace InversionIntCount
             return inversions;
         }
 
+        static int GetInversions2(List<int> integerList)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int len = integerList.Count / 2;
+            left.AddRange(integerList.GetRange(0, len));
+            right.AddRange(integerList.GetRange(len, integerList.Count - len));
+
+            if (left.Count == 1 || right.Count == 1)
+            {
+                int leftIndex = 0;
+                int rightIndex = 0;
+                int inversions = 0;
+
+                while(leftIndex < left.Count && rightIndex < right.Count)
+                {
+                    if (left[leftIndex] > right[rightIndex])
+                    {
+                        inversions++;
+                        leftIndex++;
+                    }
+                    else 
+                    {
+                        rightIndex++;
+                    }
+                }
+
+                inversions += right.Count - rightIndex;
+                return inversions;
+            }
+
+            return GetInversions2(left) + GetInversions2(right);
+        }
+
         static int Main(string[] args)
         {
             var fileName = args[0];
@@ -43,7 +78,7 @@ namespace InversionIntCount
             var totalInversions = 0;
             foreach(var line in File.ReadLines(@fileName))
             {
-                totalInversions += GetInversions(ConvertLine(line));
+                totalInversions += GetInversions2(ConvertLine(line));
             }
 
             Console.WriteLine($"Total Inversions = {totalInversions}");
@@ -55,7 +90,7 @@ namespace InversionIntCount
             var result = new List<int>();
             foreach (char i in line)
             {
-                result.Add(i);
+                result.Add(Convert.ToInt32(Char.GetNumericValue(i)));
             }
 
             return result;
