3d00d85127b0a7b9d9d7a14b4ec42121eed91474
MattMcLennan <mclennan6891@gmail.com>
2017-11-22 20:47:49 -0500

    Added setup code for parsing file

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace QuickSort
 {
+    internal enum ExitCode : int
+    {
+        Success = 0,
+        InvalidFilename = 1,
+        UnknownError = 10
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static (int totalComparisons, List<int> list) QuickSort(List<int> list)
         {
-            Console.WriteLine("Hello World!");
+
+
+            return (0, new List<int>());
+        }
+
+        static int Main(string[] args)
+        {
+            var fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                return (int)ExitCode.InvalidFilename;
+            }
+
+            List<int> inputIntegers = new List<int>();
+            foreach (var line in File.ReadLines(@fileName))
+            {
+                inputIntegers.Add(Convert.ToInt32(line));
+            }
+
+            var quickSort = QuickSort(inputIntegers);
+            Console.WriteLine($"Total Comparisons Completed = {quickSort.totalComparisons}");
+            return (int)ExitCode.Success;
         }
     }
 }
