5a8fa417bfcec35cd195edbea1e3cccece776361
MattMcLennan <mclennan6891@gmail.com>
2017-12-08 08:47:46 -0500

    Setting up framework for input file

--- KargerMinCutGraph/Program.cs
+++ KargerMinCutGraph/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace KargerMinCutGraph
 {
+    internal enum ExitCode : int
+    {
+        Success = 0,
+        InvalidFileName = 1,
+        UnknownError = 10
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                return (int)ExitCode.InvalidFileName;
+            }
+
+            // do something with the input
+
+            return (int)ExitCode.Success;
         }
     }
 }
