1db751bf5c67f964157ab577566bb037b0cd19be
MattMcLennan <mclennan6891@gmail.com>
2017-12-21 00:47:02 -0500

    Initial Graph Data Structure Set

--- Strongly-Connected-Components-SCC/Program.cs
+++ Strongly-Connected-Components-SCC/Program.cs
@@ -1,29 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Strongly_Connected_Components_SCC
 {
+    internal class Graph 
+    {
+        public Graph()
+        {
+            Vertices = new List<int>();
+            Edges = new List<Edge>();
+        }
+
+         public List<int> Vertices { get; set; }
+
+        public List<Edge> Edges { get; set; }
+   }
+
+    public class Edge
+    {
+        public int StartPoint { get; set; }
+
+        public int EndPoint { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
-            ExtractDataFromFile("TestFile.txt");
-
+            var data = ExtractDataFromFile("TestFile.txt");
         }
 
-        static void ExtractDataFromFile(string filePath)
+        static Graph ExtractDataFromFile(string filePath)
         {
+            Graph graph = new Graph();
+
             using(TextReader reader = File.OpenText(@filePath))
             {
                 string text = reader.ReadLine();
                 while(text != null)
                 {
-                    // do shit here
+                    string[] splited = text.Split(' ');
+
+                    int vertex;
+                    if (int.TryParse(splited[0], out vertex))
+                    {
+                        graph.Vertices.Add(vertex);
+
+                        int endpoint;
+                        if (int.TryParse(splited[1], out endpoint))
+                        {
+                            graph.Edges.Add(new Edge
+                            {
+                                StartPoint = vertex,
+                                EndPoint = endpoint
+                            });
+                        }
+                    }
 
                     text = reader.ReadLine();
                 }
             }
+
+            return graph;
         }
     }
 }
